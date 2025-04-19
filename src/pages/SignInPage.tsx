20fa73b8eb1a87430c6db2da8bd540ba3325488b
Shim Jeong ah <35457850+joanShim@users.noreply.github.com>
2025-04-20 08:43:37 +0900

    Feat : 로고 추가

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,17 +1,23 @@
 import { useNavigate } from '@tanstack/react-router'
 import { Button } from '@/components/ui/button'
+import logo from '@/assets/logo.png'
 
 export default function SignInPage() {
   const navigate = useNavigate()
 
   return (
     <div className="flex h-screen flex-col items-center justify-center space-y-8 p-4">
+      <img src={logo} alt="logo" className="h-40 object-cover" />
       <div className="text-center">
-        <h1 className="text-4xl font-bold">Q-SCIEN</h1>
+        {/* <h1 className="text-4xl font-bold">Q-SCIENCE</h1> */}
         <p className="mt-2 text-gray-500">과학 퀴즈로 배우는 즐거움</p>
       </div>
 
-      <Button size="lg" onClick={() => navigate({ to: '/onboarding' })}>
+      <Button
+        size="lg"
+        className="w-full"
+        onClick={() => navigate({ to: '/onboarding' })}
+      >
         시작하기
       </Button>
     </div>
